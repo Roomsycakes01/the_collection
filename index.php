d9254edbfa431b5f3d96cfee2ab64a9008dbf908
Jake Rooms <jakeprooms@outlook.com>
2021-09-30 15:09:00 +0100

    edited css

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ $objectArray = objectArray(fullQuery($db));
 <!DOCTYPE html>
 <html lang='en'>
 <head>
-    <meta charset='UTF-8'/>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+    <meta charset='UTF-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <title>The Collection</title>
-    <link rel='stylesheet' href='normalize.css'/>
-    <link rel='stylesheet' href='collection.css'/>
+    <link rel='stylesheet' href='normalize.css' />
+    <link rel='stylesheet' href='collection.css' />
 </head>
 <body>
     <h1>Home</h1>
